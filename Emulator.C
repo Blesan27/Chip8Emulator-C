00eabccd33f5f0b2a0761497ad67f5a45372f65b
Blesan27 <blesanraj@gmail.com>
2026-02-21 15:08:28 +0530

    Added Instruction Functions

--- Emulator.C
+++ Emulator.C
@@ -4,21 +4,15 @@
 #define FONTSET_SIZE 80
 #define FONTSET_START_ADDRESS 0x50
 
-//Instruction Functions
-
-#pragma region InstructionsFunction
-
+#define VIDEO_WIDTH 32
+#define VIDEO_HEIGHT 64
 
 
-#pragma endregion
 
-
-int main(){
-    
     int registers[16];
     wchar_t memory[4098];
     int index;
-    int pc;
+    uint16_t pc;
     int stack[16];
     int sp;
     int delayTimer;
@@ -26,6 +20,352 @@ int main(){
     int keypad[16];
     int video[64*32];
     int opcode;
+
+//Instruction Functions
+
+#pragma region InstructionsFunction
+
+    void OP_00E0(){
+        memset(video, 0, sizeof(video));
+    }
+
+    void OP_00EE(){
+        --sp;
+        pc = stack[sp];
+    }
+
+    void OP_1nnn(){
+        uint16_t address = opcode & 0x0FFFu;
+        pc = address;
+    }
+
+    void OP_2nnn(){
+        uint16_t address = opcode & 0x0FFFu;
+
+        stack[sp] = pc;
+        ++sp;
+        pc = address;
+    }
+
+    void OP_3xkk(){
+        uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+        uint8_t byte = opcode & 0x00FFu;
+
+        if (registers[Vx] == byte)
+        {
+            pc += 2;
+        }
+    }
+
+    void OP_4xkk(){
+        uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+        uint8_t byte = opcode & 0x00FFu;
+
+        if (registers[Vx] != byte)
+        {
+            pc += 2;
+        }
+    }
+
+    void OP_5xy0(){
+        uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+        uint8_t Vy = (opcode & 0x00F0u) >> 4u;
+
+        if (registers[Vx] == registers[Vy])
+        {
+            pc += 2;
+        }
+    }
+
+    void OP_6xkk(){
+        uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+        uint8_t byte = opcode & 0x00FFu;
+
+        registers[Vx] = byte;
+    }
+
+    void OP_7xkk(){
+        uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+        uint8_t byte = opcode & 0x00FFu;
+
+        registers[Vx] += byte;
+    }
+
+    void OP_8xy0(){
+        uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+        uint8_t Vy = (opcode & 0x00F0u) >> 4u;
+
+        registers[Vx] = registers[Vy];
+    }
+
+    void OP_8xy1(){
+        uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+        uint8_t Vy = (opcode & 0x00F0u) >> 4u;
+
+        registers[Vx] |= registers[Vy];
+    }
+
+    void OP_8xy2(){
+        uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+        uint8_t Vy = (opcode & 0x00F0u) >> 4u;
+
+        registers[Vx] &= registers[Vy];
+    }
+
+    void OP_8xy3(){
+        uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+        uint8_t Vy = (opcode & 0x00F0u) >> 4u;
+
+        registers[Vx] ^= registers[Vy];
+    }
+
+    void OP_8xy4(){
+        uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+        uint8_t Vy = (opcode & 0x00F0u) >> 4u;
+
+        uint16_t sum = registers[Vx] + registers[Vy];
+
+        if (sum > 255U){
+            registers[0xF] = 1;
+        }else{
+            registers[0xF] = 0;
+        }
+
+        registers[Vx] = sum & 0xFFu;
+    }
+
+    void OP_8xy5(){
+        uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+        uint8_t Vy = (opcode & 0x00F0u) >> 4u;
+
+        if (registers[Vx] > registers[Vy]){
+            registers[0xF] = 1;
+        }else{
+            registers[0xF] = 0;
+        }
+
+        registers[Vx] -= registers[Vy];
+    }
+
+    void OP_8xy6(){
+        uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+
+        // Save LSB in VF
+        registers[0xF] = (registers[Vx] & 0x1u);
+
+        registers[Vx] >>= 1;
+    }
+
+    void OP_8xy7(){
+        uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+        uint8_t Vy = (opcode & 0x00F0u) >> 4u;
+
+        if (registers[Vy] > registers[Vx]){
+            registers[0xF] = 1;
+        }else{
+            registers[0xF] = 0;
+        }
+
+        registers[Vx] = registers[Vy] - registers[Vx];
+    }
+
+    void OP_8xyE(){
+        uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+
+        // Save MSB in VF
+        registers[0xF] = (registers[Vx] & 0x80u) >> 7u;
+
+        registers[Vx] <<= 1;
+    }
+
+    void OP_9xy0(){
+        uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+        uint8_t Vy = (opcode & 0x00F0u) >> 4u;
+
+        if (registers[Vx] != registers[Vy]){
+            pc += 2;
+        }
+    }
+
+    void OP_Annn(){
+        uint16_t address = opcode & 0x0FFFu;
+
+        index = address;
+    }
+
+    void OP_Bnnn(){
+        uint16_t address = opcode & 0x0FFFu;
+
+        pc = registers[0] + address;
+    }
+
+    void OP_Cxkk(){
+        uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+        uint8_t byte = opcode & 0x00FFu;
+
+        registers[Vx] = randByte(ran()) & byte;
+    }
+
+    void OP_Dxyn(){
+        uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+        uint8_t Vy = (opcode & 0x00F0u) >> 4u;
+        uint8_t height = opcode & 0x000Fu;
+
+        // Wrap if going beyond screen boundaries
+        uint8_t xPos = registers[Vx] % VIDEO_WIDTH;
+        uint8_t yPos = registers[Vy] % VIDEO_HEIGHT;
+
+        registers[0xF] = 0;
+
+        for (unsigned int row = 0; row < height; ++row){
+            uint8_t spriteByte = memory[index + row];
+
+            for (unsigned int col = 0; col < 8; ++col){
+                uint8_t spritePixel = spriteByte & (0x80u >> col);
+                uint32_t* screenPixel = &video[(yPos + row) * VIDEO_WIDTH + (xPos + col)];
+
+                // Sprite pixel is on
+                if (spritePixel){
+                    // Screen pixel also on - collision
+                    if (*screenPixel == 0xFFFFFFFF){
+                        registers[0xF] = 1;
+                    }
+
+                    // Effectively XOR with the sprite pixel
+                    *screenPixel ^= 0xFFFFFFFF;
+                }
+            }
+        }
+    }
+
+    void OP_Ex9E(){
+        uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+
+        uint8_t key = registers[Vx];
+
+        if (keypad[key]){
+            pc += 2;
+        }
+    }
+
+    void OP_ExA1(){
+        uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+
+        uint8_t key = registers[Vx];
+
+        if (!keypad[key]){
+            pc += 2;
+        }
+    }
+
+    void OP_Fx07(){
+        uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+
+        registers[Vx] = delayTimer;
+    }
+
+    void OP_Fx0A(){
+        uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+
+        if (keypad[0]){
+            registers[Vx] = 0;
+        }else if (keypad[1]){
+            registers[Vx] = 1;
+        }else if (keypad[2]){
+            registers[Vx] = 2;
+        }else if (keypad[3]){
+            registers[Vx] = 3;
+        }else if (keypad[4]){
+            registers[Vx] = 4;
+        }else if (keypad[5]){
+            registers[Vx] = 5;
+        }else if (keypad[6]){
+            registers[Vx] = 6;
+        }else if (keypad[7]){
+            registers[Vx] = 7;
+        }else if (keypad[8]){
+            registers[Vx] = 8;
+        }else if (keypad[9]){
+            registers[Vx] = 9;
+        }else if (keypad[10]){
+            registers[Vx] = 10;
+        }else if (keypad[11]){
+            registers[Vx] = 11;
+        }else if (keypad[12]){
+            registers[Vx] = 12;
+        }else if (keypad[13]){
+            registers[Vx] = 13;
+        }else if (keypad[14]){
+            registers[Vx] = 14;
+        }else if (keypad[15]){
+            registers[Vx] = 15;
+        }else{
+            pc -= 2;
+        }
+    }
+
+    void OP_Fx15(){
+        uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+
+        delayTimer = registers[Vx];
+    }
+
+    void OP_Fx18(){
+        uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+
+        soundTimer = registers[Vx];
+    }
+
+    void OP_Fx1E(){
+        uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+
+        index += registers[Vx];
+    }
+
+    void OP_Fx29(){
+        uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+        uint8_t digit = registers[Vx];
+
+        index = FONTSET_START_ADDRESS + (5 * digit);
+    }
+
+    void OP_Fx33(){
+        uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+        uint8_t value = registers[Vx];
+
+        // Ones-place
+        memory[index + 2] = value % 10;
+        value /= 10;
+
+        // Tens-place
+        memory[index + 1] = value % 10;
+        value /= 10;
+
+        // Hundreds-place
+        memory[index] = value % 10;
+    }
+
+    void OP_Fx55(){
+        uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+
+        for (uint8_t i = 0; i <= Vx; ++i)
+        {
+            memory[index + i] = registers[i];
+        }
+    }
+
+    void OP_Fx65(){
+        uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+
+        for (uint8_t i = 0; i <= Vx; ++i){
+            registers[i] = memory[index + i];
+        }
+    }
+
+#pragma endregion
+
+
+int main(){
     
     int startAddress = 0x200;
     pc = startAddress;
